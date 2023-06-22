b43d56fb702647ba3b0d664c64bc5e7090afc2cc
dosepehr <dosepehr@gmail.com>
2023-06-22 15:43:36 +0330

    make Users Page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import Users from '../Pages/Users/Users';
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        children: [
+            {
+                path: '/users',
+                element: <Users />,
+            },
+        ],
     },
 ]);
 
