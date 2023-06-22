2a7e80a7b9dad76da5a1de467a0bd64862656b15
dosepehr <dosepehr@gmail.com>
2023-06-22 15:40:09 +0330

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.css'
-import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router.jsx';
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <RouterProvider router={router}>
+            <App />
+        </RouterProvider>
+    </StrictMode>
+);
+
