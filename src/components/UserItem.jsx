2f665d7ad3e86096dcdf8b14eaa7a9ef6b1e11a2
dosepehr <dosepehr@gmail.com>
2023-06-22 19:24:25 +0330

    show modal for userDetails

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,20 +1,26 @@
 import { useDeleteUserMutation } from '../redux/reducers/apiSlice';
 import { toast } from 'react-toastify';
-const UserItem = ({ username, email, _id }) => {
-    const [deleteUser, { isSuccess }] = useDeleteUserMutation();
+import Swal from 'sweetalert2';
+const UserItem = ({ username, email, _id, firstname, lastname, age, city }) => {
+    const [deleteUser] = useDeleteUserMutation();
 
     const handleDeleteUser = async (id) => {
-        await deleteUser(id).unwrap();
-        toast(`کاربر ${username} با موفقیت حذف شد💣`, {
-            position: 'top-right',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: 'light',
-            rtl: true,
+        try {
+            await deleteUser(id).unwrap();
+            toast.success(`کاربر با موفقیت حذف شد💣`);
+        } catch (err) {
+            toast.error('مشکلی به وجود آمد');
+        }
+    };
+    const handleUserDetails = () => {
+        Swal.fire({
+            icon: 'info',
+            title: `${username}`,
+            html:
+                `نام : ${firstname} <br/>` +
+                `نام خانوادگی : ${lastname} <br/>` +
+                `سن : ${age} <br/>` +
+                `شهر : ${city} <br/>`,
         });
     };
     return (
@@ -36,7 +42,10 @@ const UserItem = ({ username, email, _id }) => {
                 <button className='btn-custome btn-custome--gray'>
                     پیام ها
                 </button>
-                <button className='btn-custome btn-custome__blue'>
+                <button
+                    className='btn-custome btn-custome__blue'
+                    onClick={handleUserDetails}
+                >
                     اطلاعات
                 </button>
                 <button
