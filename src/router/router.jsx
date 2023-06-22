cdbeed9162fa21921f69cd005500aa51229f9ef9
dosepehr <dosepehr@gmail.com>
2023-06-22 15:49:40 +0330

    make Courses Page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import Users from '../Pages/Users/Users';
 import Infos from '../Pages/Infos/Infos';
+import Courses from '../Pages/Courses/Courses';
 const router = createBrowserRouter([
     {
         path: '/',
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
             {
                 path: '/infos',
                 element: <Infos/>,
+            },
+            {
+                path: '/courses',
+                element: <Courses />,
             }
         ],
     },
