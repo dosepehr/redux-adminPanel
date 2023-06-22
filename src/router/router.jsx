e9ed8a1e06b26323e7c99294f76f486c6d1fbebc
dosepehr <dosepehr@gmail.com>
2023-06-22 15:45:48 +0330

    make Infos page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import Users from '../Pages/Users/Users';
+import Infos from '../Pages/Infos/Infos';
 const router = createBrowserRouter([
     {
         path: '/',
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
                 path: '/users',
                 element: <Users />,
             },
+            {
+                path: '/infos',
+                element: <Infos/>,
+            }
         ],
     },
 ]);
